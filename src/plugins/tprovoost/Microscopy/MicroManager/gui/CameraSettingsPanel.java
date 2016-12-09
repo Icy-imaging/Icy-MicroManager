947568380fbdf9acf57ee7dd9a05b70e830029da
Tong <tongli.bioinfo@gmail.com>
2016-12-09 14:18:00 +0100

    logerror from reportingutils

--- src/plugins/tprovoost/Microscopy/MicroManager/gui/CameraSettingsPanel.java
+++ src/plugins/tprovoost/Microscopy/MicroManager/gui/CameraSettingsPanel.java
@@ -324,12 +324,12 @@ public class CameraSettingsPanel extends JPanel implements Runnable
 
     public void logError(Exception e)
     {
-        getMMStudio().logError(e);
+        ReportingUtils.logError(e);
     }
 
     public void logError(Exception e, String msg)
     {
-        getMMStudio().logError(e, msg);
+        ReportingUtils.logError(e, msg);
     }
 
     public String getCameraName()
@@ -411,7 +411,7 @@ public class CameraSettingsPanel extends JPanel implements Runnable
         }
         catch (Exception e)
         {
-            getMMStudio().logError(e);
+            ReportingUtils.logError(e);
         }
     }
 
